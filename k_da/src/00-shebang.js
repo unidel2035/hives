6aec3a56eec3291b1894d4034766773dd6bc20de
Dmitry <robhalford@list.ru>
2025-11-28 01:20:37 +0300

    фикс чата 4

--- k_da/src/00-shebang.js
+++
@@ -1,8 +0,0 @@
-/**
- * Shebang and module setup
- * Lines 1-2 from original k_da_deobfuscated.js
- * This file contains the Node.js shebang and ES module compatibility setup
- */
-
-#!/usr/bin/env node
-import { createRequire } from 'module';
